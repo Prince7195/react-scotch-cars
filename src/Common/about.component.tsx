62671a8d0603130c15ec154fcf9839f297cc101d
Vijay Deepak <vijaydeepak7195@outlook.com>
2017-11-24 14:17:43 +0530

    changes in about.component

--- src/Common/about.component.tsx
+++ src/Common/about.component.tsx
@@ -1,13 +1,22 @@
 import * as React from 'react';
 
 class About extends React.Component {
+    public state: any;
+    constructor(props: any) {
+        super(props);
+        this.state = {
+
+        }
+    }
     render() {
         return (
             <div className="container-fluid">
-                <h3>About Page</h3>
+                <H3>About Page</H3>
             </div>
         );
     }
 }
+
+const H3 = (props: any) => <h3>{props.children}</h3>;
 
 export default About;
